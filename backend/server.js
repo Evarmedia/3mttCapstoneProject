ffc17608e1f1903df651150a10063d6c48ca2d5e
Evarmedia <mishakmanuel@gmail.com>
2024-11-19 17:09:07 +0100

    fxxxxxxx

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,8 @@ const cors = require('cors');
 // Initialize the app
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 // Define the port
 const PORT = process.env.PORT || 3555;
 
